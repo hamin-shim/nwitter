12dead5e8ca2e6ecde34c8fde44166b962f9deeb
alrbs10 <89731330+alrbs10@users.noreply.github.com>
2021-11-07 23:40:43 +0900

    route!

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAYYXXStBf2W664FPXyXeY6vq9wxWUyKcY",
+    authDomain: "nwitter-dd009.firebaseapp.com",
+    projectId: "nwitter-dd009",
+    storageBucket: "nwitter-dd009.appspot.com",
+    messagingSenderId: "963081947104",
+    appId: "1:963081947104:web:7895b054da8a0293460d85"
+  };
+const app = initializeApp(firebaseConfig);
+export default app;
