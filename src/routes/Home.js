c82df0ae1d5b58a00c825eb008b9ad78510676b9
alrbs10 <89731330+alrbs10@users.noreply.github.com>
2021-11-10 12:39:11 +0900

    set collection

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { dbService } from "../nwitterFirebase";
 
-export default ()=><div>Home</div>;
+export default ()=>{
+    const [nweet, setNweet] = useState("");
+    const onSubmit = async(event)=>{
+        event.preventDefault();
+        const data = await dbService.collection("nweets").add({
+            nweet,
+            createdAt : Date.now(),
+        })
+        setNweet("");
+        console.log(data)
+    };
+    const onChange = (event)=>{
+        const {target: {value}} = event;
+        setNweet(value)
+    }
+    return(
+        <div>
+            <form onSubmit={onSubmit}>
+                <input onChange = {onChange} type="text" placeholder="what's in your mind?" value={nweet}/>
+                <input type="submit" value="nweet"/>
+            </form>
+        </div>
+)};
